c2b5ba9fa359d0b8c363ed4b2bc6ce14c5f8b144
fre <fmblom@me.com>
2019-11-28 20:05:10 +0100

    add ft_isspace to libft header and sources for Makefile

--- libft/libft.h
+++ libft/libft.h
@@ -41,6 +41,7 @@ int					ft_isdigit(int c);
 int					ft_isprint(int c);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
+int					ft_isspace(int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strequ(char const *s1, char const *s2);
