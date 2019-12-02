6c460bea741d0222a3a9db9870935ca4f46f93d5
fre <fmblom@me.com>
2019-12-02 11:54:35 +0100

    print list of instructions

--- asm/src/print_list.c
+++ asm/src/print_list.c
@@ -22,15 +22,40 @@ void		print_list(t_list **list)
 	items = 0;
 	while (item != NULL)
 	{
-		if (item->content_size == 72)
+		if (item->content_size == sizeof(t_op))
 		{
 			ft_printf("instruction: %s\n", (((t_op *)(item->content))->name));
 			ft_printf("arg1: %i\n", (((t_op *)(item->content))->argtypes[0]));
 			ft_printf("arg2: %i\n", (((t_op *)(item->content))->argtypes[1]));
 			ft_printf("arg2: %i\n", (((t_op *)(item->content))->argtypes[2]));
+			if ((((t_op *)(item->content))->argvalues[0].label))
+			{
+				ft_printf("value1: %s\n", (((t_op *)(item->content))->argvalues[0].label));
+				free((((t_op *)(item->content))->argvalues[0].label));
+			}
+			else
+				ft_printf("value1: %i\n", (((t_op *)(item->content))->argvalues[0].value));
+			if ((((t_op *)(item->content))->argvalues[1].label))
+			{
+				ft_printf("value2: %s\n", (((t_op *)(item->content))->argvalues[1].label));
+				free((((t_op *)(item->content))->argvalues[1].label));
+			}
+			else
+				ft_printf("value2: %i\n", (((t_op *)(item->content))->argvalues[1].value));
+			if ((((t_op *)(item->content))->argvalues[2].label))
+			{
+				ft_printf("value3: %s\n", (((t_op *)(item->content))->argvalues[2].label));
+				free((((t_op *)(item->content))->argvalues[2].label));
+			}
+			else
+				ft_printf("value3: %i\n", (((t_op *)(item->content))->argvalues[2].value));
+
 		}
 		else
-			ft_printf("label: %s\n", (((t_op *)item->content)));
+		{
+			if (item->content)
+				ft_printf("label: %s\n", (((t_op *)item->content)));
+		}
 		temp = item;
 		item = item->next;
 		free (temp->content);
