68cc04d03f8fcc0beffd0177f9d551770ce81e47
sandRiches <richard.corke@hotmail.co.uk>
2019-12-12 12:13:49 +0100

    merge conflict fix

--- vm/src/parse_player.c
+++ vm/src/parse_player.c
@@ -6,16 +6,19 @@
 /*   By: rcorke <rcorke@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/11 14:11:10 by rcorke         #+#    #+#                */
-/*   Updated: 2019/11/13 15:59:01 by rcorke        ########   odam.nl         */
+/*   Updated: 2019/12/12 12:00:39 by rcorke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "vm.h"
+#include "../include/vm.h"
 
 /*
 ** Checks that player file has correct info then parses it into struct
 */
+// 1100 0010 0000 1001
+// 1001 0000 0010 1100 == MAGICE HEADER
 
+//
 static int	check_magic(t_player *player, char *str, int *i)
 {
 	if (str[*i] != 0)
