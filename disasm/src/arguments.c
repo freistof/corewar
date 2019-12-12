b0e7b1330675b55464230c85e861b3ba4f1567e7
fre <fmblom@me.com>
2019-12-12 13:30:19 +0100

    do not use unsigned data types

--- disasm/src/arguments.c
+++ disasm/src/arguments.c
@@ -27,7 +27,7 @@ static int		extract_bit(int n, int k, int p)
 
 static int		write_register(int fd_in, int fd_out)
 {
-	unsigned char		reading;
+	char				reading;
 	char				*number;
 
 	read(fd_in, &reading, 1);
@@ -40,18 +40,21 @@ static int		write_register(int fd_in, int fd_out)
 
 static int		write_indirect(int fd_in, int fd_out)
 {
-	unsigned short		reading;
+	short		reading;
+	char		*number;
 
 	read(fd_in, &reading, 2);
 	reading = swap_bits_two_byte(reading);
-	write(fd_out, &reading, 1);
+	number = ft_itoa(reading);
+	write(fd_out, number, ft_strlen(number));
+	free(number);
 	return (2);
 }
 
 static int		write_direct(int fd_in, int fd_out, int opcode)
 {
-	unsigned int		reading;
-	unsigned short		readshort;
+	int					reading;
+	short				readshort;
 	char				*number;
 	int					size;
 
