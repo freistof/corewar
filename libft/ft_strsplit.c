8e31c6ee159ce2751c0c3d4341e5d4fb160188b3
Freistof <fmblom@me.com>
2020-02-21 15:00:40 +0100

    fix invalid write in split

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -16,35 +16,30 @@ static char		**make_temp(char **string, const char *s, char c)
 {
 	char	**temp;
 
-	string[ft_substring_count(s, c)] = NULL;
+       	string[ft_substring_count(s, c) - 1] = NULL;
 	temp = string;
 	return (temp);
 }
 
 static char		**put_in_string(char *substring, char **string)
 {
-	int		i;
-
-	i = 0;
 	*string = substring;
 	return (string += 1);
 }
 
 char			**ft_strsplit(char const *s, char c)
 {
-	size_t		j;
 	char		*substring;
 	char		**string;
 	char		**temp;
 
 	string = (char **)malloc((sizeof(char *) + 1) *
-		ft_substring_count(s, c) + 1);
+		ft_substring_count(s, c));
 	if (string == NULL)
 		return (string);
 	temp = make_temp(string, s, c);
 	while (*s != '\0')
 	{
-		j = 0;
 		if (*s == c)
 			s++;
 		else
